021b1ee596f989625305ec863fbef35398ded743
Rupesh Chidupudi <chrupesh2425@gmail.com>
2024-11-24 23:42:32 +0530

    Initial commit

--- Shell/src/main/ShellEmulator.java
+++ Shell/src/main/ShellEmulator.java
@@ -1,10 +1,10 @@
 package main;
 
+import java.util.Scanner;
 import utils.CommandExecutor;
 
-import java.util.Scanner;
-
 public class ShellEmulator {
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String currentDir = System.getProperty("user.dir"); // Start with the current directory
