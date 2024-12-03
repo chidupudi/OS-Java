fb4b307c7a473396e67544504baff1384ea33b0a
Rupesh Chidupudi <chrupesh2425@gmail.com>
2024-12-04 00:38:56 +0530

    Initial commit

--- Shell/src/main/ShellEmulator.java
+++ Shell/src/main/ShellEmulator.java
@@ -6,19 +6,15 @@ public class ShellEmulator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String currentDir = System.getProperty("user.dir"); // Start with the current directory
-
         System.out.println("Welcome to the Basic Shell Emulator!");
         System.out.println("Type 'exit' to quit.\n");
-
         while (true) {
             System.out.print(currentDir + " > ");
             String input = scanner.nextLine();
-
             if (input.equalsIgnoreCase("exit")) {
                 System.out.println("Exiting Shell. Goodbye!");
                 break;
             }
-
             try {
                 currentDir = CommandExecutor.executeCommand(input, currentDir);
             } catch (Exception e) {
